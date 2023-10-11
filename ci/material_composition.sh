be4237a120bb91a61866f94a84154086991550b2
Paul Gessinger <paul.gessinger@cern.ch>
2023-10-11 15:24:15 +0200

    fix

--- ci/material_composition.sh
+++ ci/material_composition.sh
@@ -5,8 +5,8 @@ output=$2
 
 acts-install/bin/ActsAnalysisMaterialComposition \
       -i $input \
-      -o $output/material_composition.root \
+      -o $output \
       --config ci/composition_config.json \
       -s
 
-ci/make_detector_plot.py ci/composition_config.json $output/layout.pdf
+ci/make_material_plots.py ci/composition_config.json $output/layout.pdf
