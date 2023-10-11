c1d36620dc38de38e259bcace54791201056993b
Anna Zaborowska <anna.zaborowska@cern.ch>
2023-10-11 10:45:53 +0200

    Add endcap to material budget

--- ci/material_composition.sh
+++ ci/material_composition.sh
@@ -6,9 +6,9 @@ output=$2
 acts-install/bin/ActsAnalysisMaterialComposition \
       -i $input \
       -o $output \
-      --sub-names beampipe pixel sstrips lstrips solenoid ecalbarrel \
-      --sub-rmin 0:25:202:720:1160:1250 \
-      --sub-rmax 24.4:200:720:1140:1200:1500 \
-      --sub-zmin -4000:-2400:-3150:-3150:-3000:-3050 \
-      --sub-zmax 4000:2400:3150:3150:3000:3050 \
+      --sub-names beampipe pixel sstrips lstrips solenoid ecalbarrel ecalendcap\
+      --sub-rmin 0:25:202:720:1160:1250:315 \
+      --sub-rmax 24.4:200:720:1140:1200:1500:1500 \
+      --sub-zmin -4000:-2400:-3150:-3150:-3000:-3050:3200 \
+      --sub-zmax 4000:2400:3150:3150:3000:3050:3450 \
       -s
