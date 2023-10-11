589dd4646b50beddd6b81585fe1809fe2eafcadc
Anna Zaborowska <anna.zaborowska@cern.ch>
2023-10-11 13:55:37 +0200

    Extend ecal envelopes and matscan dimensions

--- ci/material_composition.sh
+++ ci/material_composition.sh
@@ -8,7 +8,7 @@ acts-install/bin/ActsAnalysisMaterialComposition \
       -o $output \
       --sub-names beampipe pixel sstrips lstrips solenoid ecalbarrel ecalrightendcap ecalleftendcap\
       --sub-rmin 0:25:202:720:1160:1250:315:315 \
-      --sub-rmax 24.4:200:720:1140:1200:1500:1500:1500 \
-      --sub-zmin -4000:-2400:-3150:-3150:-3000:-3050:3200:-3450 \
-      --sub-zmax 4000:2400:3150:3150:3000:3050:3450:-3200 \
+      --sub-rmax 24.4:200:720:1140:1200:1540:1540:1540 \
+      --sub-zmin -4000:-2400:-3150:-3150:-3000:-3050:3200:-3500 \
+      --sub-zmax 4000:2400:3150:3150:3000:3050:3500:-3200 \
       -s
