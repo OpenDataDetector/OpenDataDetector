3720c3c2f9ddd5e61a0db5d12ae7b2604cdc45b8
Dimitra Amperiadou <dimitraamp.1997@gmail.com>
2024-02-12 12:49:36 +0100

    update muon geometry and assign sensitive elements

--- factory/muons/ODDMuonBarrel_geo.cpp
+++ factory/muons/ODDMuonBarrel_geo.cpp
@@ -5,10 +5,11 @@
 //
 // Mozilla Public License Version 2.0
 
-
-
 #include "DD4hep/DetFactoryHelper.h"
 #include "XML/Utilities.h"
+
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 using namespace dd4hep;
@@ -17,138 +18,157 @@ using namespace dd4hep;
 ///
 /// @param oddd the detector to which this is addedded
 /// @param xml the input xml element
-/// @param sens is ignored
+/// @param sens the sensitive detector
 ///
 /// @return a reference counted DetElement
 
-static Ref_t create_element(Detector &oddd, xml_h xml,  SensitiveDetector sens){
+static Ref_t create_element(Detector &oddd, xml_h xml, SensitiveDetector sens)
+{
 	xml_det_t x_det = xml;
 	string detName = x_det.nameStr();
 
-	// Construct the detector element 
+	// Construct the detector element
 	DetElement barrelMuonDetector(detName, x_det.id());
 	dd4hep::xml::setDetectorTypeFlag(xml, barrelMuonDetector);
 
-	// Make Volume
-  	dd4hep::xml::Dimension x_det_dim(x_det.dimensions());
-  	string barrelShapeName = x_det_dim.nameStr();	
-	
-    xml_comp_t x_ch = x_det.child(_Unicode(chamber));
-    xml_comp_t x_ml = x_ch.child(_Unicode(multilayer));
-    xml_comp_t x_tb = x_ml.child(_U(tubs));
-    xml_comp_t x_tlayer = x_ml.child(_U(layer)); 
+	dd4hep::xml::Dimension x_det_dim(x_det.dimensions());
+	string barrelShapeName = x_det_dim.nameStr();
 
-      //Parameters for the volumes
-    double rmin = x_det_dim.rmin();
-    //The shape and volume 
+	// The shape and volume
 	Tube barrelMuonShape(x_det_dim.rmin(), x_det_dim.rmax(), x_det_dim.dz());
-	Volume barrelMuonVolume(detName, barrelMuonShape,oddd.air());	   
-        		
-    //The radial distance on xy for the big and small barrel chambers
-    //small chambers are placed above the big ones
-	double rb = sqrt(pow(x_ch.x(),2) + pow(rmin+x_ch.dy(),2));
-	double rsm = sqrt(pow(x_ch.x(),2) + pow(rmin+x_ch.dy() + 2*x_ch.dy(),2));
+	Volume barrelMuonVolume(detName, barrelMuonShape, oddd.air());
 
-    double phistep = 2*M_PI/x_det_dim.nphi();
-    double phi0 = 0.5*M_PI; 
+	// The identifiers for the chambers and the gas-tubes
+	unsigned int chamberNum = 1;
+	unsigned int tubeNum = 1;
 
-       
-    for(int i=0; i<x_det_dim.nphi(); i++){
+	std::string tubeName, chamberName;
 
-        double phi = phi0 + i*phistep;
+	for (xml_coll_t layer(x_det, _U(layer)); layer; ++layer)
+	{
 
-        //position of the big barrel chambers
+		xml_comp_t x_l = layer;
 
-		double x=rb*cos(phi);
-		double y=rb*sin(phi);
+		xml_comp_t x_ch = x_l.child(_Unicode(chamber));
+		xml_comp_t x_gas = x_ch.child(_U(tubs));
+		xml_comp_t x_shell = x_ch.child(_Unicode(shell));
 
-		 //z position for side A and side B
-		double za = -x_ch.dz();
-		double zb = -za;	
-
-		//big  chambers dimensions
-		double ch_dx = x_ch.dx();
-		double ch_dy = x_ch.dy();
-		double ch_dz = x_ch.dz(); 
-		double rtubemax = x_tb.rmax1();
-		double rtubemin = x_tb.rmin1();
-		double zstep = 2*x_ch.dz() + x_det_dim.z_offset();
-		string name = "MDT_Chamber_Big";
-
-		//Switch to small barrel chambers when i is even
-        if(i%2 == 0){
-
-        	//position of the small barrel chambers
-			x = rsm*cos(phi);
-			y = rsm*sin(phi);
-			//small chambers dimensions
-			ch_dx*=0.8;
-			ch_dy*=0.8;
-			ch_dz*=0.8;
-			rtubemax=x_tb.rmax2();
-			rtubemin=x_tb.rmin2();
-			name = "MDT_Chamber_Small";
+		if (x_gas.isSensitive())
+		{
+			sens.setType("tracker");
 		}
+		// small chambers are placed above the big ones along y
+		double rb = sqrt(pow(x_ch.x(), 2) + pow(x_l.rmin() + x_ch.dy(), 2));
+		double rsm = sqrt(pow(x_ch.x(), 2) + pow(x_l.rmin() + x_ch.dy() + 2 * x_ch.dy() + x_ch.y_offset(), 2));
 
-		for(int j=0; j<x_det_dim.nz(); j++){
-			        
+		double phistep = 2 * M_PI / x_l.nphi();
+		double phi0 = 0.5 * M_PI;
 
-			//Build the chamber, the multilayer and the tube layers with the tubes
-			Box chBox(ch_dx,ch_dy,ch_dz);
-			Volume chVolume(name, chBox, oddd.air());
-			chVolume.setVisAttributes(oddd,x_ch.visStr());	
-			//Each multilayer has 0.4 of the height of the chamber
-			//we need to multilayers with some space between them
-			Box mlBox(ch_dx, 0.4*ch_dy, ch_dz);
-            Volume mlVolume("MDT_MultiLayer", mlBox, oddd.air());
-            mlVolume.setVisAttributes(oddd,x_ml.visStr()); 
-            //the number of tubes along x dimension          
-            int ntubes = mlBox.x()/rtubemax;
-            Tube driftTubeShape(rtubemin, rtubemax, mlBox.z());
-            Volume driftTubeVolume(x_tb.nameStr(), driftTubeShape, oddd.air());
-            driftTubeVolume.setVisAttributes(oddd,x_tb.visStr());
-            
-            for(int nl=0; nl<x_tlayer.repeat(); nl++){                
-                
-                for(int t=0; t<ntubes; t++){
+		for (int i = 0; i < x_l.nphi(); i++)
+		{
 
-                	mlVolume.placeVolume(driftTubeVolume,Transform3D(Position(-mlBox.x()+(2*t+1)*rtubemax, -mlBox.y()+(2*nl+1)*rtubemax,0 )));   
-                  
-                }
-            }
+			double phi = phi0 + i * phistep;
 
-            chVolume.placeVolume(mlVolume, Transform3D(Position(0, - chBox.y() + mlBox.y(),0))); 
-            chVolume.placeVolume(mlVolume, Transform3D(Position(0, + chBox.y() - mlBox.y(),0)));
+			// Define the volumes' parameters
 
+			// position of the big barrel chambers
+			double x = rb * cos(phi);
+			double y = rb * sin(phi);
 
-			barrelMuonVolume.placeVolume(chVolume, Transform3D(RotationZ(i*phistep),Position(x,y,za)));
-			barrelMuonVolume.placeVolume(chVolume, Transform3D(RotationZ(i*phistep),Position(x,y,zb)));
+			// z position for side A and side B
+			double za = -x_ch.dz() - x_l.z_offset();
+			double zb = -za;
 
-			//move along z for the next chambers
-			za = za - zstep;
-			zb = zb + zstep;            
-            
-        }
+			// big  chambers dimensions
+			double ch_dx = x_ch.dx();
+			double ch_dy = x_ch.dy();
+			double ch_dz = x_ch.dz();
+			double rtubemax = x_gas.rmax();
+			double rtubemin = x_gas.rmin();
+			double rshellmin = x_shell.rmin();
+			double rshellmax = x_shell.rmax();
+			double zstep = 2 * x_ch.dz() + x_l.z_offset();
 
-       
-    }             
-        
+			// Switch to small barrel chambers when i is even
+			if (i % 2 == 0)
+			{
 
-  //visualize the barrel cylinder
-  barrelMuonVolume.setVisAttributes(oddd, x_det.visStr());
+				// position of the small barrel chambers
+				x = rsm * cos(phi);
+				y = rsm * sin(phi);
+				// small chambers dimensions
+				ch_dx *= 0.9;
+				ch_dy *= 0.9;
+				ch_dz *= 0.9;
+				rtubemax *= 0.9;
+				rtubemin *= 0.9;
+				rshellmin *= 0.9;
+				rshellmax *= 0.9;
+			}
 
-  // Place Volume
-  Volume motherVolume = oddd.pickMotherVolume(barrelMuonDetector);
-  Position translation(0., 0., x_det_dim.z());
+			// Build the chamber,  and the tube layers with the tubes
+			Box chBox(ch_dx, ch_dy, ch_dz);
+			Tube driftTubeShape(rtubemin, rtubemax, chBox.z());
+			Tube ShellTubeShape(rshellmin, rshellmax, chBox.z());
+			Volume driftShellVolume(x_shell.nameStr(), ShellTubeShape, oddd.material(x_shell.materialStr()));
+			driftShellVolume.setVisAttributes(oddd, x_shell.visStr());
+			int ntubes = (chBox.x() - rshellmax) / rshellmax;
+			double ygap = 2 * (ch_dy - 2 * (sqrt(3) * rshellmax + rshellmax));
 
-  PlacedVolume placedMuonBarrel = motherVolume.placeVolume(barrelMuonVolume, translation);
-  placedMuonBarrel.addPhysVolID("system", barrelMuonDetector.id());
-  barrelMuonDetector.setPlacement(placedMuonBarrel);
+			// Place the tubes and the chambers
+			for (int j = 0; j < x_l.nz(); j++)
+			{
 
-  return barrelMuonDetector;
-		
+				// place the chambers once in side A and once in side B
+				chamberName = _toString((int)chamberNum, "chamber%d");
+				double z = (j % 2) ? za : zb;
+				Volume chVolume(chamberName, chBox, oddd.air());
+				chVolume.setVisAttributes(oddd, x_ch.visStr());
+				DetElement chamberElement(barrelMuonDetector, chamberName, chamberNum);
+				PlacedVolume pv = barrelMuonVolume.placeVolume(chVolume, Transform3D(RotationZ(i * phistep), Position(x, y, z)));
+				pv.addPhysVolID(x_ch.nameStr(), chamberNum++);
+				chamberElement.setPlacement(pv);
+
+				for (int nl = 0; nl < x_gas.repeat(); nl++)
+				{
+
+					// put the tubes (sensitive gas+shell) in two multilayers with ygap distance between
+					double yt = -chBox.y() + rshellmax + nl * sqrt(3) * rshellmax;
+					if (nl >= 0.5 * x_gas.repeat())
+						yt += ygap;
+
+					for (int t = 0; t < ntubes; t++)
+					{
+
+						tubeName = _toString((int)tubeNum, "tube%d");
+						Volume driftTubeVolume(tubeName, driftTubeShape, oddd.material(x_gas.materialStr()));
+						driftTubeVolume.setVisAttributes(oddd, x_gas.visStr());
+						driftTubeVolume.setSensitiveDetector(sens);
+						PlacedVolume pvtube = chVolume.placeVolume(driftTubeVolume, Transform3D(Position(-chBox.x() + (2 * t + 1) * rshellmax + (nl % 2) * rshellmax, yt, 0)));
+						chVolume.placeVolume(driftShellVolume, Transform3D(Position(-chBox.x() + (2 * t + 1) * rshellmax + (nl % 2) * rshellmax, yt, 0)));
+						pvtube.addPhysVolID("tube", tubeNum++);
+						DetElement sensTubeEl(chamberElement, tubeName, tubeNum);
+						sensTubeEl.setPlacement(pvtube);
+					}
+				}
+
+				// move along z for the next chambers
+				(j % 2) ? za -= zstep : zb += zstep;
+			}
+		}
+	}
+
+	// visualize the barrel cylinder
+	barrelMuonVolume.setVisAttributes(oddd, x_det.visStr());
+
+	// Place Volume
+	Volume motherVolume = oddd.pickMotherVolume(barrelMuonDetector);
+	Position translation(0., 0., x_det_dim.z());
+
+	PlacedVolume placedMuonBarrel = motherVolume.placeVolume(barrelMuonVolume, translation);
+	placedMuonBarrel.addPhysVolID("system", x_det.id());
+	barrelMuonDetector.setPlacement(placedMuonBarrel);
+	return barrelMuonDetector;
 }
 
 DECLARE_DETELEMENT(ODDMuonBarrel, create_element)
-
-
--- factory/muons/ODDMuonEndCap_geo.cpp
+++ factory/muons/ODDMuonEndCap_geo.cpp
@@ -4,14 +4,11 @@
 //
 // Mozilla Public License Version 2.0
 
-
-
 #include "DD4hep/DetFactoryHelper.h"
 #include "XML/Utilities.h"
 
 using namespace std;
 using namespace dd4hep;
-
 
 /// Standard create_element(...) create muon spectrometer endcap like geometry
 ///
@@ -21,7 +18,7 @@ using namespace dd4hep;
 ///
 /// @return a reference counted DetElement
 
-static Ref_t create_element(Detector &oddd, xml_h xml,  SensitiveDetector sens){
+static Ref_t create_element(Detector &oddd, xml_h xml, SensitiveDetector sens){
 
 	xml_det_t x_det = xml;
 	string detName = x_det.nameStr();
@@ -30,106 +27,122 @@ static Ref_t create_element(Detector &oddd, xml_h xml,  SensitiveDetector sens){
 	dd4hep::xml::setDetectorTypeFlag(xml, endcapMuonDetector);
 
 	// Make Volume
-  	dd4hep::xml::Dimension x_det_dim(x_det.dimensions());
-  	string endcapShapeName = x_det_dim.nameStr();
-	
-	xml_comp_t x_tb = x_det.child(_U(tubs));
-		
-	double phi0 = 0.5*M_PI;
-	double phistep = 2*M_PI/x_det_dim.nphi();
-	double rmin = x_det_dim.rmin();
-	double r = rmin;
-	double rtubemin = x_tb.rmin();
-	double rtubemax = x_tb.rmax();
+	dd4hep::xml::Dimension x_det_dim(x_det.dimensions());
+	string endcapShapeName = x_det_dim.nameStr();
 
-	//The shape and volume 
+	// The shape and volume
 	Tube endcapMuonShape(x_det_dim.rmin(), x_det_dim.rmax(), x_det_dim.dz());
-	Volume endcapMuonVolume(detName, endcapMuonShape,oddd.air());
+	Volume endcapMuonVolume(detName, endcapMuonShape, oddd.air());
 
-	size_t chamberNum = 0;
+	unsigned int chamberNum = 1;
+	unsigned int tubeNum = 1;
+	unsigned int layerNum = 1;
 
-	for(xml_coll_t chamber(x_det, _Unicode(chamber)); chamber; chamber++, chamberNum++){
+	std::string tubeName, chamberName, layerName;
 
-		xml_comp_t x_ch = chamber;
+	for (xml_coll_t layer(x_det, _Unicode(layer)); layer; layer++){
 
-		//the radial position of the chamber
-		r += x_ch.dy();
+		xml_comp_t x_l = layer;
+		xml_comp_t x_gas = x_l.child(_Unicode(tubs));
+		xml_comp_t x_shell = x_l.child(_Unicode(shell));
 
-		for(int i=0; i<x_det_dim.nphi(); i++){
-
-			double phi = phi0 + i*phistep;
-
-			//position of chamber on z
-			double z = x_ch.zmax();
-
-			if(i%2==0){
-
-				 z = x_ch.zmin();				 
-			}
-
-			Box chBox(x_ch.dx(), x_ch.dy(), x_ch.dz());
-			Volume chVolume(x_ch.nameStr(), chBox, oddd.air());
-			chVolume.setVisAttributes(oddd,x_ch.visStr());	
-
-			//position of the chamber volumes
-			double x = r*cos(phi);
-			double y = r*sin(phi);
-
-			//place the tubes inside the chambers
-			Tube driftTubeShape(rtubemin, rtubemax, x_ch.dy());
-			Volume driftTubeVolume(x_tb.nameStr(), driftTubeShape, oddd.air());
-			driftTubeVolume.setVisAttributes(oddd, x_tb.visStr());
-
-			//the number of tubes along -x included the offset of one radius
-			int ntubesx = (x_ch.dx()-rtubemax)/(rtubemax);
-			//loop over the layers along z
-			double zt = -x_ch.dz() + rtubemax;
-			for(int iz=0; iz<x_tb.nz(); iz++){
-
-				double xt = -x_ch.dx() + x_tb.x_offset();
-				if(iz%2!=0){
-					xt = -x_ch.dx() + 2*x_tb.x_offset();
-				}
-				//loop over the tubes along x
-				for(int ix=0; ix<ntubesx; ix++){
-				
-					chVolume.placeVolume(driftTubeVolume, Transform3D(RotationX(0.5*M_PI),Position(xt,0,zt)));
-					//shift for the next tube -along x
-					xt+=2*x_tb.x_offset();
-
-				}
-				//shift for the next tubes layer -along z
-				zt+=x_tb.z_offset();
-
-			}
-
-			endcapMuonVolume.placeVolume(chVolume, Transform3D(RotationZ(i*phistep),Position(x,y,z)));
-
-
+		if (x_gas.isSensitive()){
+			sens.setType("tracker");
 		}
 
-		//reach the top plane of the chamber -to move to the next chamber
-		r+=x_ch.dy();
+		double phi0 = 0.5 * M_PI;
+		double phistep = 2 * M_PI / x_l.nphi();
+		double r = x_l.rmin();
+		layerName = _toString((int)layerNum, "layer%d");
+		Tube layerMuonShape(x_l.rmin(), x_l.rmax(), x_l.dz());
+		Volume layerMuonVolume(layerName, layerMuonShape, oddd.air());
+		PlacedVolume pvlayer = endcapMuonVolume.placeVolume(layerMuonVolume, Position(0, 0, x_l.z()));
+		pvlayer.addPhysVolID("layer", layerNum++);
+		DetElement layerElement(endcapMuonDetector, layerName, layerNum);
+		layerElement.setPlacement(pvlayer);
 
+		for (xml_coll_t chamber(x_l, _Unicode(chamber)); chamber; chamber++){
 
+			xml_comp_t x_ch = chamber;
+
+			// the radial position of the chamber
+			r += x_ch.dy();
+
+			// shape of the chamber and the tubes of each layer
+			Tube driftTubeShape(x_gas.rmin(), x_gas.rmax(), x_ch.dy());
+			Tube shellTubeShape(x_shell.rmin(), x_shell.rmax(), x_ch.dy());
+			Box chBox(x_ch.dx(), x_ch.dy(), x_ch.dz());
+
+			for (int i = 0; i < x_l.nphi(); i++){
+
+				double phi = phi0 + i * phistep;
+
+				// position of chamber
+				double z = (i % 2) ? x_ch.zmax() : x_ch.zmin();
+				double x = r * cos(phi);
+				double y = r * sin(phi);
+
+				// create the volumes for the chambers and place them
+				chamberName = _toString((int)chamberNum, "chamber%d");
+				Volume chVolume(chamberName, chBox, oddd.air());
+				chVolume.setVisAttributes(oddd, x_ch.visStr());
+				PlacedVolume pvchamber = layerMuonVolume.placeVolume(chVolume, Transform3D(RotationZ(i * phistep), Position(x, y, z)));
+				pvchamber.addPhysVolID("chamber", chamberNum++);
+				DetElement chamberElement(layerElement, chamberName, chamberNum);
+				chamberElement.setPlacement(pvchamber);
+
+				// loop over the tube layers along z
+				double zt = -x_ch.dz() + x_shell.rmax();
+				for (int iz = 0; iz < x_gas.nz(); iz++){
+					int ntubesx = x_ch.dx() / (x_shell.rmax());
+					double xt = -x_ch.dx() + x_gas.x_offset();
+
+					if (iz % 2 != 0){
+						xt += x_gas.x_offset();
+						ntubesx -= 1;
+					}
+					// loop over the tubes along x
+					for (int ix = 0; ix < ntubesx; ix++){
+
+						// create and place the tubes inside the chambers (gas+shell)
+						tubeName = _toString((int)tubeNum, "tube%d");
+						Volume driftTubeVolume(tubeName, driftTubeShape, oddd.air());
+						driftTubeVolume.setVisAttributes(oddd, x_gas.visStr());
+						driftTubeVolume.setSensitiveDetector(sens);
+						PlacedVolume pvtube = chVolume.placeVolume(driftTubeVolume, Transform3D(RotationX(0.5 * M_PI), Position(xt, 0, zt)));
+						pvtube.addPhysVolID("tube", tubeNum++);
+						DetElement tubeElement(chamberElement, tubeName, tubeNum);
+						tubeElement.setPlacement(pvtube);
+
+						Volume shellTubeVolume(x_shell.nameStr(), shellTubeShape, oddd.air());
+						shellTubeVolume.setVisAttributes(oddd, x_shell.visStr());
+						chVolume.placeVolume(shellTubeVolume, Transform3D(RotationX(0.5 * M_PI), Position(xt, 0, zt)));
+
+						// shift for the next tube -along x
+						xt += 2 * x_gas.x_offset();
+					}
+					// shift for the next tubes layer -along z
+					zt += x_gas.z_offset();
+				}
+			}
+
+			// reach the top plane of the chamber -to move to the next chamber
+			r += x_ch.dy();
+		}
 	}
 
+	// visualize the barrel cylinder
+	endcapMuonVolume.setVisAttributes(oddd, x_det.visStr());
 
-	//visualize the barrel cylinder
-  endcapMuonVolume.setVisAttributes(oddd, x_det.visStr());
+	// Place Volume
+	Volume motherVolume = oddd.pickMotherVolume(endcapMuonDetector);
+	Position translation(0., 0., x_det_dim.z());
 
-  // Place Volume
-  Volume motherVolume = oddd.pickMotherVolume(endcapMuonDetector);
-  Position translation(0., 0., x_det_dim.z());
+	PlacedVolume placedMuonEndCap = motherVolume.placeVolume(endcapMuonVolume, translation);
+	placedMuonEndCap.addPhysVolID("system", endcapMuonDetector.id());
+	endcapMuonDetector.setPlacement(placedMuonEndCap);
 
-  PlacedVolume placedMuonEndCap = motherVolume.placeVolume(endcapMuonVolume, translation);
-  placedMuonEndCap.addPhysVolID("system", endcapMuonDetector.id());
-  endcapMuonDetector.setPlacement(placedMuonEndCap);
-
-  return endcapMuonDetector;
-		
-
-
+	return endcapMuonDetector;
 }
 
 DECLARE_DETELEMENT(ODDMuonEndCap, create_element)
