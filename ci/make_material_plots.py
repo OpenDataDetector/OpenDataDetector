c7d8ef0d963e98610a00e2e83f5fe359dcefafc3
Andreas Stefl <andreas.stefl@cern.ch>
2026-03-04 13:57:57 +0000

    replace uproot with root for material plot

--- ci/make_material_plots.py
+++ ci/make_material_plots.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-import uproot
 import matplotlib.pyplot as plt
-import numpy as np
-import os
-import hist
 import mplhep
 import argparse
 from pathlib import Path
 import json
+import math
+import ROOT
+
+from root_helper import TH1
+
 
 plt.rcParams["ytick.right"] = plt.rcParams["xtick.top"] = True
 plt.rcParams["xtick.direction"] = "in"
@@ -24,14 +25,14 @@ plt.rcParams["legend.handlelength"] = 1.0
 
 
 parser = argparse.ArgumentParser(description="Make material composition plots")
-parser.add_argument("input", help="Input root file with histograms")
+parser.add_argument("input", type=Path, help="Input root file with histograms")
 parser.add_argument("config", type=Path, help="Input config file with colours")
 parser.add_argument("output", type=Path, help="Output directory for plots")
 args = parser.parse_args()
 
 args.output.mkdir(parents=True, exist_ok=True)
 
-rf = uproot.open(args.input)
+rf = ROOT.TFile.Open(args.input.absolute().as_posix())
 
 tracker = {
     "beampipe": "Beam pipe",
@@ -50,51 +51,49 @@ calo = {
 
 with args.config.open() as f:
     config = json.load(f)
-configcolours = {det: config[det][0]["colour"] for det in config.keys() }
+configcolours = {det: config[det][0]["colour"] for det in config.keys()}
 
 for group, names in [
     ("tracker_", {**tracker}),
     ("calo_", {**calo}),
     ("", {**tracker, **calo}),
 ]:
-    for q in (
-        "x0", 
-        "l0",
-    ):
-        for qq in ("phi", "eta"):
-            h = None
-            hists = {}
+    for y in ("x0", "l0"):
+        for x in ("phi", "eta"):
+            x_lim = {"phi": (-math.pi, math.pi), "eta": (-4, 4)}[x]
+
+            hists = []
+            bins = None
             labels = []
-            detector = None
-            for k in rf:
-                name, _ = k.split(";", 1)
-                if not name.endswith("all"): continue
-                if not q in name: continue
-                if not qq in name: continue
-                if name.startswith("detector"):
-                    detector = values.copy()
-                    continue
+            colours = []
+            for name, label in names.items():
+                key = f"{name}_{y}_vs_{x}_all"
+                if rf.Get(key) is not None:
+                    th1 = TH1(rf.Get(key), xrange=x_lim)
+                    edges = list(th1.x_lo) + [th1.x_hi[-1]]
 
-                o = rf[k].to_hist()
-                o.axes[0].label = qq
-                l, _ = k.split("_", 1)
-                if not l in names:
-                    continue
-                hists[l] = o
-                l = names[l]
+                    hists.append(th1.y)
+                    bins = edges
+                    labels.append(label)
+                    colours.append(configcolours[name])
+                else:
+                    print(f"Key {key} not found in {args.input}")
 
-            labels = [v for k, v in names.items() if k in hists]
-            colours  = [configcolours[k] for k in names.keys() if k in hists]
-            hists = [hists[k] for k in names.keys() if k in hists]
-                
             fig, ax = plt.subplots()
-            mplhep.histplot(hists, ax=ax, stack=True, histtype="fill", label=labels, color=colours)
+            mplhep.histplot(
+                hists,
+                bins=bins,
+                ax=ax,
+                stack=True,
+                histtype="fill",
+                label=labels,
+                color=colours,
+            )
             ymin, ymax = ax.get_ylim()
-            ax.set_ylim(top=1.2*ymax)
+            ax.set_ylim(top=1.2 * ymax)
             ax.legend(ncol=3)
 
-            ylab = {"l0": "\lambda_i", "x0": "X_0"}[q]
+            ylab = {"l0": r"\lambda_0", "x0": "X_0"}[y]
             ax.set_ylabel(rf"${ylab}$")
-            ax.set_xlim(hists[0].axes[0].edges[0], hists[0].axes[0].edges[-1])
-            fig.savefig(args.output / f"{group}{q}_vs_{qq}.pdf")
-
+            ax.set_xlim(bins[0], bins[-1])
+            fig.savefig(args.output / f"{group}{y}_vs_{x}.pdf")
